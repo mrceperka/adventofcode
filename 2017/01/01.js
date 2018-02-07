420bf233273b0e7ca3d13425ee73ac6bbcc832b8
Mr Ceperka <johnnychudomsky@gmail.com>
2018-02-07 22:36:43 +0100

    Update 01.js

--- 2017/01/01.js
+++ 2017/01/01.js
@@ -10,3 +10,19 @@ const f = (list) => {
   }  
   return sum
 }
+
+// second solution
+const f2sum = (arr) => {
+  const arrCopy = arr.slice()
+  if (arr.length) {
+    return arrCopy.shift() + f2sum(arrCopy)
+  }
+  return 0
+}
+
+const f2 = (list) => {
+  const listLength = list.length
+  return f2sum(list.filter((el, i) => {
+    return el === list[(i + 1 ) % listLength]
+  }))
+}
